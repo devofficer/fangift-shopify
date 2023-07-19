644b22dbc5bcd75a44fe18031be95b1aeb0252b4
devofficer <sompaqueen47@gmail.com>
2023-07-18 19:17:02 -0700

    feat: complete slider component

--- src/collection.js
+++ src/collection.js
@@ -4,5 +4,12 @@ $(function () {
     min: 0,
     max: 500,
     values: [75, 300],
+    slide: function (_event, ui) {
+      $("#amount").html(`$${ui.values[0]} - $${ui.values[1]}`);
+    },
   });
+
+  const val0 = $("#slider-range").slider("values", 0);
+  const val1 = $("#slider-range").slider("values", 1);
+  $("#amount").html(`$${val0} - $${val1}`);
 });

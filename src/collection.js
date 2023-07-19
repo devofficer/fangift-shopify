f3b4946b5b2ffc37c7579676ab0aecf7ccdc5e9e
devofficer <sompaqueen47@gmail.com>
2023-07-18 20:47:31 -0700

    feat: implement filter box component

--- src/collection.js
+++ src/collection.js
@@ -12,4 +12,14 @@ $(function () {
   const val0 = $("#slider-range").slider("values", 0);
   const val1 = $("#slider-range").slider("values", 1);
   $("#amount").html(`$${val0} - $${val1}`);
+
+  $(".btn-filter-toggle").on("click", function () {
+    if ($(this).hasClass("toggled")) {
+      $(this).removeClass("toggled");
+      $(this).closest(".filter-box").removeClass("toggled");
+    } else {
+      $(this).addClass("toggled");
+      $(this).closest(".filter-box").addClass("toggled");
+    }
+  });
 });

c4ffffab9f3f22e1fd0cd7b44d8a43d61571c1c2
devofficer <sompaqueen47@gmail.com>
2023-08-14 15:43:50 -0400

    feat: show loading spinner at first loading

--- src/scripts/pages/app.js
+++ src/scripts/pages/app.js
@@ -1,5 +1,6 @@
 import fangiftService from "../services/fangiftService";
 import LINKS from "../constants/links";
+import spinner from "../utils/snip";
 
 $(function () {
   const isPublicPage = Object.values(LINKS)
@@ -9,9 +10,15 @@ $(function () {
   if (isPublicPage) {
     $("body").removeClass("hidden");
   } else {
+    const overlay = $('<div class="h-screen w-screen"></div>');
+    $("html").append(overlay);
+    spinner.spin(overlay[0]);
+
     fangiftService.get("/auth").then((userInfo) => {
       if (userInfo) {
         $("body").removeClass("hidden");
+        spinner.stop();
+        overlay.detach();
       }
     });
   }

84f6c96ff11fb2e30dfb897b8e2acb38f344b0ff
devofficer <sompaqueen47@gmail.com>
2023-08-27 21:59:58 -0400

    feat: implement landing hero section

--- src/scripts/constants/links.js
+++ src/scripts/constants/links.js
@@ -1,5 +1,8 @@
 const LINKS = {
   home: { path: "/" },
+  vendor: { path: "/pages/become-a-vendor", public: true },
+  publicWishlist: { path: "/pages/public-wishlist", public: true },
+  howItWorks: { path: "/pages/how-it-works", public: true },
   login: { path: "/account/login", public: true },
   register: { path: "/account/register", public: true },
   account: { path: "/pages/user" },
